c8b216c9ffc8b8cc33f00d2272511f5534daf8ff
Alex Hilton <dragonhilton@gmail.com>
2013-10-17 21:27:58 +0800

    Save and restore current tab between sessions.

--- src/com/hilton/vulcan/TabsFragment.java
+++ src/com/hilton/vulcan/TabsFragment.java
@@ -1,5 +1,8 @@
 package com.hilton.vulcan;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -14,6 +17,8 @@ import android.widget.TextView;
 
 public class TabsFragment extends Fragment {
     protected static final String TAG = TabsFragment.class.getName();
+    private static final String PREFS = "default_prefs";
+    private static final String CURRENT_TAB = "current_tab";
     private Tab[] mTabs = new Tab[] {
 	    new Tab("Tab 1"),
 	    new Tab("Tab 2"),
@@ -25,12 +30,32 @@ public class TabsFragment extends Fragment {
     private TabHost mTabHost;
     
     @Override
+    public void onDestroy() {
+	super.onDestroy();
+	saveCurrentTab();
+    }
+
+    private void saveCurrentTab() {
+	SharedPreferences sp = getActivity().getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+	Editor ed = sp.edit();
+	ed.putInt(CURRENT_TAB, mTabHost.getCurrentTab());
+	ed.commit();
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
 	    Bundle savedInstanceState) {
 	View root = inflater.inflate(R.layout.tabs, container, false);
 	mTabHost = (TabHost) root.findViewById(android.R.id.tabhost);
 	setupTabs(mTabHost);
+	restoreCurrentTab();
 	return root;
+    }
+
+    private void restoreCurrentTab() {
+	SharedPreferences sp = getActivity().getSharedPreferences(PREFS, Context.MODE_PRIVATE);
+	final int current = sp.getInt(CURRENT_TAB, 0);
+	mTabHost.setCurrentTab(current);
     }
     
     private void setupTabs(final TabHost tabHost) {
