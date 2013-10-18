a396790af1b40d2705de79985c48a7de211d5515
Alex Hilton <dragonhilton@gmail.com>
2013-10-18 21:41:26 +0800

    Formatting codes.
    Replace tab with spaces.

--- src/com/hilton/vulcan/MainActivity.java
+++ src/com/hilton/vulcan/MainActivity.java
@@ -8,15 +8,15 @@ public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-	// Inflate the menu; this adds items to the action bar if it is present.
-	getMenuInflater().inflate(R.menu.main, menu);
-	return true;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
 }
--- src/com/hilton/vulcan/TabsFragment.java
+++ src/com/hilton/vulcan/TabsFragment.java
@@ -22,95 +22,96 @@ public class TabsFragment extends Fragment {
     private static final String PREFS = "default_prefs";
     private static final String CURRENT_TAB = "current_tab";
     private Tab[] mTabs = new Tab[] {
-	    new Tab("Tab 1", "Home", R.drawable.home),
-	    new Tab("Tab 2", "Bookmarks", R.drawable.favorite_bookmark),
-	    new Tab("Tab 3", "Favorites", R.drawable.star),
-	    new Tab("Tab 4", "Users", R.drawable.user),
-	    new Tab("Tab 5", "Bastkets", R.drawable.shopping_basket)
-    };
-    
+            new Tab("Tab 1", "Home", R.drawable.home),
+            new Tab("Tab 2", "Bookmarks", R.drawable.favorite_bookmark),
+            new Tab("Tab 3", "Favorites", R.drawable.star),
+            new Tab("Tab 4", "Users", R.drawable.user),
+            new Tab("Tab 5", "Bastkets", R.drawable.shopping_basket) };
+
     private TabHost mTabHost;
     private LayoutInflater mFactory;
-    
 
     @Override
     public void onAttach(Activity activity) {
-	super.onAttach(activity);
-	mFactory = LayoutInflater.from(activity);
+        super.onAttach(activity);
+        mFactory = LayoutInflater.from(activity);
     }
 
     @Override
     public void onDestroy() {
-	super.onDestroy();
-	saveCurrentTab();
+        super.onDestroy();
+        saveCurrentTab();
     }
 
     private void saveCurrentTab() {
-	SharedPreferences sp = getActivity().getSharedPreferences(PREFS, Context.MODE_PRIVATE);
-	Editor ed = sp.edit();
-	ed.putInt(CURRENT_TAB, mTabHost.getCurrentTab());
-	ed.commit();
+        SharedPreferences sp = getActivity().getSharedPreferences(PREFS,
+                Context.MODE_PRIVATE);
+        Editor ed = sp.edit();
+        ed.putInt(CURRENT_TAB, mTabHost.getCurrentTab());
+        ed.commit();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	    Bundle savedInstanceState) {
-	View root = inflater.inflate(R.layout.tabs, container, false);
-	mTabHost = (TabHost) root.findViewById(android.R.id.tabhost);
-	setupTabs(mTabHost);
-	restoreCurrentTab();
-	return root;
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.tabs, container, false);
+        mTabHost = (TabHost) root.findViewById(android.R.id.tabhost);
+        setupTabs(mTabHost);
+        restoreCurrentTab();
+        return root;
     }
 
     private void restoreCurrentTab() {
-	SharedPreferences sp = getActivity().getSharedPreferences(PREFS, Context.MODE_PRIVATE);
-	final int current = sp.getInt(CURRENT_TAB, 0);
-	mTabHost.setCurrentTab(current);
+        SharedPreferences sp = getActivity().getSharedPreferences(PREFS,
+                Context.MODE_PRIVATE);
+        final int current = sp.getInt(CURRENT_TAB, 0);
+        mTabHost.setCurrentTab(current);
     }
-    
+
     private void setupTabs(final TabHost tabHost) {
-	tabHost.setup();
-	for (final Tab tab : mTabs) {
-	    TabSpec spec = tabHost.newTabSpec(tab.mTag);
-	    View indicator = mFactory.inflate(R.layout.tab_indicator, null, false);
-	    ImageView icon = (ImageView) indicator.findViewById(R.id.tab_icon);
-	    icon.setImageResource(tab.mIcon);
-	    TextView title = (TextView) indicator.findViewById(R.id.tab_title);
-	    title.setText(tab.mTitle);
-	    spec.setIndicator(indicator);
-	    spec.setContent(new TabContentFactory() {
-		@Override
-		public View createTabContent(String tag) {
-		    TextView tv =  new TextView(getActivity());
-		    tv.setText(tab.mTitle);
-		    return tv;
-		}
-		
-	    });
-	    tabHost.addTab(spec);
-	}
-	tabHost.setOnTabChangedListener(new OnTabChangeListener() {
-	    @Override
-	    public void onTabChanged(String tabId) {
-		Log.e(TAG, "which is showing ? tab id " + tabId);
-	    }
-	});
+        tabHost.setup();
+        for (final Tab tab : mTabs) {
+            TabSpec spec = tabHost.newTabSpec(tab.mTag);
+            View indicator = mFactory.inflate(R.layout.tab_indicator, null,
+                    false);
+            ImageView icon = (ImageView) indicator.findViewById(R.id.tab_icon);
+            icon.setImageResource(tab.mIcon);
+            TextView title = (TextView) indicator.findViewById(R.id.tab_title);
+            title.setText(tab.mTitle);
+            spec.setIndicator(indicator);
+            spec.setContent(new TabContentFactory() {
+                @Override
+                public View createTabContent(String tag) {
+                    TextView tv = new TextView(getActivity());
+                    tv.setText(tab.mTitle);
+                    return tv;
+                }
+
+            });
+            tabHost.addTab(spec);
+        }
+        tabHost.setOnTabChangedListener(new OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                Log.e(TAG, "which is showing ? tab id " + tabId);
+            }
+        });
     }
-    
+
     private class Tab {
-	public String mTag;
-	public String mTitle;
-	public int mIcon;
-	
-	public Tab(final String tag, String title, int icon) {
-	    mTag = tag;
-	    mTitle = title;
-	    mIcon = icon;
-	}
-	
-	@Override
-	public String toString() {
-	    return "[tag " + mTag + "]";
-	}
+        public String mTag;
+        public String mTitle;
+        public int mIcon;
+
+        public Tab(final String tag, String title, int icon) {
+            mTag = tag;
+            mTitle = title;
+            mIcon = icon;
+        }
+
+        @Override
+        public String toString() {
+            return "[tag " + mTag + "]";
+        }
     }
 }
